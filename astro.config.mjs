6487223538f87f8a9573b47006ec9656c3064bea
korlavik <korlavik@fel.cvut.cz>
2026-02-26 10:55:28 +0100

    Accessibility, sitemap, SEO optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.logopedieandelpages.dev',
   output: 'static',
   adapter: cloudflare({
     imageService: 'compile',
   }),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
